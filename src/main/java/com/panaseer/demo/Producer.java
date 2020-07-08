a911c73582baf982d3efaba1cf35abbd00f754b3
Arjun Toor <38423910+arjuntoor1@users.noreply.github.com>
2020-07-08 17:47:25 +0100

    Cleanup props

--- src/main/java/com/panaseer/demo/Producer.java
+++ src/main/java/com/panaseer/demo/Producer.java
@@ -2,7 +2,6 @@ package com.panaseer.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -15,12 +14,8 @@ public class Producer {
     private static final Logger LOG = LoggerFactory.getLogger(Producer.class);
 
     private final KafkaTemplate<String, String> kafkaTemplate;
-    private final String topic;
 
-    public Producer(@Value("${app.kafka.producer.topic:arj-test}") String topic,
-                    KafkaTemplate<String, String> kafkaTemplate,
-                    @Value("${arj.hello:did not work}") String arj) {
-        this.topic = topic;
+    public Producer(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
@@ -32,6 +27,6 @@ public class Producer {
     private void send(int value) {
         String message = String.format("%d - %s", value, DateTimeFormatter.ISO_INSTANT.format(Clock.systemUTC().instant()));
         LOG.info("Publishing message {}", message);
-        kafkaTemplate.send(topic, message);
+        kafkaTemplate.send("com-panaseer-demo", message);
     }
 }
